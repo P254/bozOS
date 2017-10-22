68adae3991e351256e40450ccac846f5585d51c1
abaner10 <abaner10@illinois.edu>
2017-10-22 02:54:19 -0500

    added headers and magic numbers to functional i8259 and rtc handler

--- student-distrib/RTC_handler.c
+++ student-distrib/RTC_handler.c
@@ -1,32 +1,29 @@
 #include "lib.h"
 #include "RTC_handler.h"
 #include "i8259.h"
+#include "exceptions.h"
 
-#define RTC_IRQ_ADDR 8
-
-/*http://wiki.osdev.org/RTC*/
+/* source from http://wiki.osdev.org/RTC
+ * Inputs: void
+ * Return Value: void
+ * Function: Initialize real time clock and add RTC to IDT*/
 void rtc_init(void){
-  /*Not entirely sure what this means???
-  Be sure that you install the IRQ handler
-  before you enable the RTC IRQ.
-  The interrupt will happen almost immediately.*/
-  //cli();
-  char  previous;
-  outb(0x8B, 0x70); //select status register B and disable interuppts using x80.
-  previous = inb(0x71); //	  read immediately after or the RTC may be left in an unknown state.
-  outb(0x8B, 0x70);
-  outb(previous | 0x40, 0x71);  // write the previous value ORed with 0x40. This turns on bit 6 of register B
+  char previous;
+  outb(NMI_MASK+REG_B, RTC_REG); //select status register B and disable interuppts using x80.
+  previous = inb(RTC_REG+1); //	  read immediately after or the RTC may be left in an unknown state.
+  outb(NMI_MASK+REG_B, RTC_REG);
+  outb(previous | BIT_6_SWITCH, RTC_REG+1);  // write the previous value ORed with 0x40. This turns on bit 6 of register B
   enable_irq(RTC_IRQ_ADDR); //enable 8th IRQ
   set_IDT_wrapper(SOFT_INT_START + RTC_IRQ_ADDR, rtc_handler);
-
-  /*sti();*/
-  outb(0x0B, 0x70);  //enable 80 bit NMI?? IDK
+  outb(REG_B, RTC_REG);  //enable 80 bit NMI
 }
 
+/* Inputs: void
+ * Return Value: void
+ * Function: Handles RTC interrupts by call test_interrupts and sending EOI signal to PIC */
 void rtc_handler(void){
-  printf("RTC handler call\n");
-  // test_interrupts(); //we must prove this function is being called.
+  test_interrupts(); //we must prove this function is being called.
   send_eoi(RTC_IRQ_ADDR); //end 8th IRQ
-  outb(0x0C, 0x70);	// select register C
-  inb(0x71);		// just throw away contents, we must do this otherwise IRQ8 will never be called again.
+  outb(REG_C, RTC_REG);	// select register C
+  inb(RTC_REG+1);		// just throw away contents, we must do this otherwise IRQ8 will never be called again.
 }
--- student-distrib/RTC_handler.h
+++ student-distrib/RTC_handler.h
@@ -3,6 +3,16 @@
 
 #include "types.h"
 
+/*Magic numbers*/
+#define NMI_MASK 0x80
+#define REG_A 0x0A
+#define REG_B 0x0B
+#define REG_C 0x0C
+#define RTC_REG 0x70
+#define RTC_IRQ_ADDR 8
+#define BIT_6_SWITCH 0x40
+
+/*forward declarations*/
 void rtc_init(void);
 void rtc_handler(void);
 
