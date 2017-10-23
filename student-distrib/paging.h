496e09b17ee78c5e29fdf5eac925a9c8b44b4017
Lee <slee500@illinois.edu>
2017-10-23 00:42:47 -0500

    Paging sorta works, but need to fix the page fault exception.

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -2,12 +2,11 @@
 #define PAGING_H
 
 #define ALIGN_4KB 4096
-#define N_PDE 1024 // A 4GB page directory supports 1024 entries of 4KB page table entries
-#define N_PTE 1024 // A 4 MiB page table has 1024 entries
+#define PAGE_SIZE 1024 
 
 #define VIDEO_MEM 0xB8000 // Taken from lib.c
-#define KERNEL_MEM 0x400000
+#define KERNEL_MEM 0x400000 // Corresponds to 4MB
 
 void paging_init();
 
-#endif  PAGING_H
+#endif PAGING_H
