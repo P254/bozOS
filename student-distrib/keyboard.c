7a98583691ff48e90fb5ebf605f73e3934c98cdd
Lee <slee500@illinois.edu>
2017-10-23 15:39:32 -0500

    Add comments for keyboard

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -6,7 +6,7 @@
 #include "x86_desc.h"
 // scancode taken from osdever.com
 
-unsigned char scancode[128] =
+unsigned char scancode[KB_SIZE] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
   '9', '0', '-', '=', '\b',	/* Backspace */
@@ -46,15 +46,29 @@ unsigned char scancode[128] =
     0,	/* All other keys are undefined */
 };
 
+/*
+ * kb_init
+ *   DESCRIPTION: Main function that initializes the keyboard interrupt. 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: void
+ *   SIDE EFFECTS: modifies the KB entry in the IDT
+ */
 void kb_init(void){
-    enable_irq(1); // the keyboard interrupt
+    enable_irq(KB_IRQ); // the keyboard interrupt
     set_IDT_wrapper(SOFT_INT_START + 1, get_char);
-    // set_idt_reserved(&idt[i]);
 }
 
 
-
-char getScancode() {
+/*
+ * getScanCode
+ *   DESCRIPTION: Grabs the scancode from the keyboard and returns the code if it is non-zero.
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: char -- the character scanned in from the keyboard
+ *   SIDE EFFECTS: none
+ */
+char getScanCode() {
     char c = 0 ;
     do {
         if (inb(KEYBOARD_PORT) != c ) {
@@ -64,11 +78,18 @@ char getScancode() {
     } while(1);
 }
 
+/*
+ * get_char
+ *   DESCRIPTION: Grabs the character and prints it to the screen
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: void
+ *   SIDE EFFECTS: Prints a character to screen
+ */
 void get_char() {
     // we have to use this somewhere to print to the screen.
     // outb smthing
-
     send_eoi(1);
-    putc(scancode[(int)getScancode()]);
+    putc(scancode[(int)getScanCode()]);
     while(1);
 }
