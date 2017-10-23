8c6a446cd1927c478dad031b2bf39e0c29baa7b5
Lee <slee500@illinois.edu>
2017-10-23 12:32:48 -0500

    Code refactoring and comments

--- student-distrib/IDT.h
+++ student-distrib/IDT.h
@@ -5,9 +5,11 @@
 #define EMPTY_EXCEPTION 15
 #define N_EXCEPTIONS 20
 #define N_EXCEPTIONS_RESERVED 32
+#define USER_INT_START 32
+#define SYS_CALL 0x80
 
 /*Forward Declerations*/
-void init_idt_exceptions();
 void set_IDT_wrapper(uint8_t idt_num, void* handler_function);
+void init_IDT();
 
 #endif /* IDT_H */
