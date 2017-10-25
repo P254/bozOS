27d7cb703b6190c6a08ac12f2ad3d8e80d8a5d6b
Lee <slee500@illinois.edu>
2017-10-24 22:51:15 -0500

    Removed the while(1) loop from C exception handlers. An interesting observation: excluding cli() means we can still type/do RTC stuff, while excluding asm("hlt") gives a GP exception.

--- student-distrib/IDT.c
+++ student-distrib/IDT.c
@@ -80,13 +80,12 @@ void init_IDT() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e0() {
     printf("Interrupt 0 - Divide Error Exception (#DE) \n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -95,13 +94,12 @@ void handle_e0() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e1() {
     printf("Interrupt 1 - Debug Exception (#DB) \n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -110,13 +108,12 @@ void handle_e1() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e2() {
     printf("Interrupt 2 - NMI Interrupt\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -125,13 +122,12 @@ void handle_e2() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e3() {
     printf("Interrupt 3 - Breakpoint Exception (#BP)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -140,13 +136,12 @@ void handle_e3() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e4() {
     printf("Interrupt 4 - Overflow Exception (#OF)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -155,13 +150,12 @@ void handle_e4() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e5() {
     printf("Interrupt 5 - BOUND Range Exceeded Exception (#BR)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -170,13 +164,12 @@ void handle_e5() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e6() {
     printf("Interrupt 6 - Invalid Opcode Exception (#UD)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -185,13 +178,12 @@ void handle_e6() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e7() {
     printf("Interrupt 7 - Device Not Available Exception (#NM)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -200,13 +192,12 @@ void handle_e7() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e8() {
     printf("Interrupt 8 - Double Fault Exception (#DF)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -215,7 +206,7 @@ void handle_e8() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e9() {
     // Exception Class Abort.
@@ -223,7 +214,6 @@ void handle_e9() {
     printf("Interrupt 9 - Coprocessor Segment Overrun\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -232,13 +222,12 @@ void handle_e9() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e10() {
     printf("Interrupt 10 - Invalid TSS Exception (#TS)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -247,13 +236,12 @@ void handle_e10() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e11() {
     printf("Interrupt 11 - Segment Not Present (#NP)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -262,13 +250,12 @@ void handle_e11() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e12() {
     printf("Interrupt 12 - Stack Fault Exception (#SS)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -277,13 +264,12 @@ void handle_e12() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e13() {
     printf("Interrupt 13 - General Protection Exception (#GP)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -292,13 +278,12 @@ void handle_e13() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e14() {
     printf("Interrupt 14 - Page-Fault Exception (#PF)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -307,13 +292,12 @@ void handle_e14() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e15() {
     printf("Interrupt 15 - Reserved\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -322,13 +306,12 @@ void handle_e15() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e16() {
     printf("Interrupt 16 - x87 FPU Floating-Point Error (#MF)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -337,13 +320,12 @@ void handle_e16() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e17() {
     printf("Interrupt 17 - Alignment Check Exception (#AC)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -352,13 +334,12 @@ void handle_e17() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e18() {
     printf("Interrupt 18 - Machine-Check Exception (#MC)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -367,13 +348,12 @@ void handle_e18() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_e19() {
     printf("Interrupt 19 - SIMD Floating-Point Exception (#XF)\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -382,13 +362,12 @@ void handle_e19() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_default() {
     printf("Default interrupt handler called. Nothing specified here.\n");
     cli();
     asm("hlt");
-    while(1);
 }
 
 /*
@@ -398,11 +377,10 @@ void handle_default() {
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: void
- *   SIDE EFFECTS: spins in a while(1) infinite loop
+ *   SIDE EFFECTS: masks interrupts, halts system
  */
 void handle_sys_call() {
     printf("System call.\n");
     cli();
     asm("hlt");
-    while(1);
 }
