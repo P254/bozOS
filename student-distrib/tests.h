1d0f3ea2cc2a62a50e7c3dacedea0e8b19a35f48
Lee <slee500@illinois.edu>
2017-10-30 13:43:06 -0500

    Clean up RTC_handler and tests

--- student-distrib/tests.h
+++ student-distrib/tests.h
@@ -1,18 +1,10 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-#define VID_MEM 0xB8000
-#define KERN_MEM 0x400000
-#define SIZE_TABLE 1024
-#define SIZE_TABLE 1024
-#define IDT_SIZE 256
-#define VID_MEM_LOC 0xB8000
-#define KERN_MEM 0x400000
-#define FILLED_LOC 2
-#define TEXT 0 
-#define NONTEXT 1
+// decide which tests we want to launch
+#define RTC_TEST_ENABLE 1
 
-// test launcher
+// test-launching function
 void launch_tests();
 
 #endif /* TESTS_H */
