d05c3257c2a2e122bbdef49d7198e38fb7939c5f
Lee <slee500@illinois.edu>
2017-11-17 18:51:58 -0600

    Fixing the merge that I messed up

--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -51,7 +51,7 @@ int32_t read_data(uint32_t inode, uint32_t offset, uint8_t *buf, uint32_t length
 
 int32_t fopen(const uint8_t *fname);
 int32_t fclose(uint8_t *fname);
-int32_t fread(uint8_t *fname, uint8_t *buf, int32_t nbytes);
+int32_t fread(uint8_t fd, uint8_t *buf, int32_t nbytes);
 int32_t fwrite(void);
 
 int32_t dopen(const uint8_t* fname, dentry_t *dentry);
@@ -59,5 +59,7 @@ int32_t dread(uint8_t fd, uint8_t *buf, int32_t nbytes);
 
 int32_t dclose();
 int32_t dwrite(void);
+
+extern volatile int process_number;
 
 #endif
