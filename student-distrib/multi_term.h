0b3d27f8fe9fa608b8b389525a1e71c1f4635f3f
slee500 <slee500@illinois.edu>
2017-12-06 12:14:37 -0600

    Revert "Merge branch 'kush' into 'sean'"
    This reverts commit 7921635e1ed7b40779fd1cf0dafff1ff46e4d8c5

--- student-distrib/multi_term.h
+++ student-distrib/multi_term.h
@@ -10,6 +10,10 @@
 #define TERM_3      2
 #define TERM_SIZE   3
 
+#define COLOR_1    0x7
+#define COLOR_2    0xA 
+#define COLOR_3    0xC 
+
 enum pu_t {NOT_USED, IN_USE};
 
 typedef struct term {
@@ -20,14 +24,13 @@ typedef struct term {
     int x;                  // X-position for printing
     int y;                  // Y-position for printing
     int color;              // Makes it easier to see which terminal I'm in
-    uint32_t* vidmap_addr;  // For use with vidmap
+    // uint32_t* vidmap_addr;  // For use with vidmap
 } term_t;
 
 /*Forward declarations*/
 pcb_t* get_PCB_tail(uint8_t terminal_n);
 term_t* get_terminal_ptr(uint8_t terminal_n);
 void switch_terminal(uint8_t new_terminal);
-void copy_terminal(uint8_t new_terminal);
 void multi_term_init();
 void reset_pcb_head(uint8_t terminal_n);
 void unset_process_usage(uint8_t pid);
